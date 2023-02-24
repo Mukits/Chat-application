d45cf3ba0cc71878d0bc49f16234ffb2113a043d
mukit <mukiturahma99@hotmail.com>
2023-02-24 01:24:09 +0000

    signup form html and css

--- controllers/users.js
+++ controllers/users.js
@@ -3,9 +3,14 @@ module.exports = function(_){
 return{
     SetRouting: function(router){
         router.get('/',this.indexPage);
+        router.get('/signup',this.getSignUp);
     },
     indexPage: function(req,res){
         return res.render('index',{test:'this is a test'});
+    },
+
+    getSignUp: function(req,res){
+        return res.render('signup')
     }
 }
 }
