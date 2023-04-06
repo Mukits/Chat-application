186ad703a5cd353aac6da41a5e8ef5b80ad0f390
mukits <mukiturahma99@hotmail.com>
2023-04-06 21:47:02 +0100

    Connection when user enters a chat room

--- public/js/group/groupchat.js
+++ public/js/group/groupchat.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
+    // io variable made possible for use thanks to the socket.io.js imported in group.ejs in views
+    var socket = io();
+
+    socket.on('connect',function(){
+        //will be displayed on the browser
+        console.log('user connected successfully')
+    });
+
 
 });
--- socketio/groupchat.js
+++ socketio/groupchat.js
@@ -1,3 +1,7 @@
 module.exports = function(io){
-    
+    //for the connection io.on will listen to the connection
+    io.on('connection',(socket)=>{
+        //will be displayed on the console
+        console.log('user connected');
+    });
 }
