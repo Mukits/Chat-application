827f72e540555d08c5e8e08e5843706e1577cce7
mukits <mukiturahma99@hotmail.com>
2023-04-08 23:49:53 +0100

    sending and listening to pm

--- public/js/privatecht/privatemex.js
+++ public/js/privatecht/privatemex.js
@@ -9,6 +9,32 @@ $(document).ready(function(){
     console.log('1',newParam);
     swap(newParam,0,1);
     console.log('2', newParam)
+    var paramTwo = newParam[0]+'.'+newParam[1];
+
+    socket.on('connect',function(){
+        var params = {
+            room1: paramOne,
+            room2: paramTwo
+        }
+        socket.emit('join private',params, function(){
+        console.log('user joined pm');
+        });
+
+        $('#message_form').on('submit', function (e) {
+            // to prevent the form to reload after its submitted
+            e.preventDefault();
+            //gets the msg value from the input field in group.ejs
+            var msg = $('#msg').val();
+            var sender = $('#name-user').val();
+            // make sure that user cannot send empty msg trim cancels spaces
+            if(msg.trim().length > 0){
+                socket.emit('private message',{
+                    text: msg,
+                    sender:sender
+                })
+            }
+        });
+    });
 });
 
 function swap(input, value_1, value_2){
--- socketio/privatemessage.js
+++ socketio/privatemessage.js
@@ -1,6 +1,19 @@
 module.exports = function(io)
 {
     io.on('connection',  (socket)=>{
+        socket.on('join private',(pm, callback)=>{
+            // any user that pathname is equal to room 1 and room2 wil automatically join this event and will listen to this event then they can message each other 
+            socket.join(pm.room1);
+            socket.join(pm.room2);
+            // to test that use has joined pm
+            callback();
 
+
+        });
+
+        socket.on('private message', (message)=>{
+            // if this is successfull we will see the name and text of the sender
+            console.log(message);
+        })
     });
 }
