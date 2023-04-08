ddd64527f37eaaff46808466eb2a9fbcc004d6aa
mukits <mukiturahma99@hotmail.com>
2023-04-08 21:18:32 +0100

    users can now use the filter by function

--- controllers/searchResults.js
+++ controllers/searchResults.js
@@ -1,11 +1,36 @@
-module.exports = function()
-{
+module.exports = function(async, group){
     return {
         SetRouting: function(router){
-            router.get('/searchResults',this.getsearchResults)
+            router.get('/searchResults', this.getResults);
+            router.post('/searchResults', this.postResults);
+            
+            
         },
-        getsearchResults: function(req,res){
-            res.render('searchResults',{user: req.user});
+        
+        getResults: function(req, res){
+            res.redirect('/home');
+        },
+        
+        postResults: function(req, res){
+            async.parallel([
+                function(callback){
+                    const regex = new RegExp((req.body.country), 'gi');
+                    
+                    group.find({'$or': [{'country':regex}, {'name': regex}]}, (err, result) => {
+                       callback(err, result); 
+                    });
+                }
+            ], (err, results) => {
+                const res1 = results[0];
+                
+                const dataChunk  = [];
+                const chunkSize = 3;
+                for (let i = 0; i < res1.length; i += chunkSize){
+                    dataChunk.push(res1.slice(i, i+chunkSize));
+                }
+                
+                res.render('searchResults', {title: 'Chat-application - searchResults', user: req.user, parts: dataChunk});
+            })
         }
     }
 }
