a055d353eb2baa55891c42e0c367a60fab09da98
mukits <mukiturahma99@hotmail.com>
2023-04-08 17:59:19 +0100

    displaying online friends

--- public/js/global.js
+++ public/js/global.js
@@ -15,4 +15,22 @@ $(document).ready(function () {
 
 
     });
+    socket.on('loggedInUser',function(users){
+        var friends=$('.friend').text();
+        var friend = friends.split('@');
+
+        var name = $ ('#name-user').val();
+        var ol = $('<div></div>');
+
+        var arr = [];
+        for(var i = 0; i < users.length; i++){
+            if(friend.indexOf(users[i].name) > -1){
+                arr.push(users[i]);
+                ol.append(users[i].name);
+
+            }
+        }
+        $('#numOfFriends').text('('+arr.length+')');
+        $('.onlineFriends').html(ol);
+    });
 });
--- socketio/globalrooms.js
+++ socketio/globalrooms.js
@@ -8,8 +8,9 @@ module.exports = function(io, Global, _){
             clients.EnterRoom(socket.id, global.name, global.room, global.img);
             
             const nameProp = clients.GetRoomList(global.room);
+            // returns a list of unique properties deleting duplicates
             const arr = _.uniqBy(nameProp, 'name');
-            
+            // emitting the arra to all the users connected to global room containing name and image
             io.to(global.room).emit('loggedInUser', arr);
         });
         
