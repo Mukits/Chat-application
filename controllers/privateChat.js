caefd43adc40193b4ce6079751e1db7601407fcb
mukits <mukiturahma99@hotmail.com>
2023-04-09 14:10:59 +0100

    fetching the last message
    now when a user receives a message from a another user he will receive the notification but only 1 notification will be shown since we are fetching only the last message between the two users, if the user receives a message from a new user that value will be incremented by 1

--- controllers/privateChat.js
+++ controllers/privateChat.js
@@ -15,13 +15,57 @@ module.exports = function(async, Users,Message){
                         .exec((err, result) => {
                             callback(err, result);
                         })
+                },
+
+                // aggregate method to process data records and return the computed results
+                function(callback){
+                    // to avoid complexity for uppercase and lowercase usernames
+                    const nameRegex = new RegExp("^"+req.user.username.toLowerCase(), "i");
+                    Message.aggregate([
+                        // it will match and search for every document inside the message collection where senderName 
+                        // and receiverName is equal to user.username
+                        {$match: {$or: [{'senderName': nameRegex},
+                        {'receiverName': nameRegex}]}},
+                        // sort the data
+                        {$sort:{'createdAt':-1}},
+                        {
+                            $group: { "_id":{
+                                "last_message_between": { 
+                                    // mongodb condition operator to return onl;y data we need
+                                    $cond : [
+                                        {
+                                            // greater than operator mongodb
+                                            $gt: [
+                                                // substr to get data from index 0 to 1 so get the senderName and receiverName
+                                                {$substr:["$senderName",0,1]},
+                                                {$substr:["$receiverName",0,1]}]
+                                        },
+                                        // contatenating the senderName and receiverName
+                                        {$concat:["$senderName"," and ","$receiverName"]},
+                                        {$concat:["$receiverName"," and ","$senderName"]}
+                                        
+                                    ]
+                                }
+                                // the body will hold all the data processed above, root means the currently processed document
+                            }, "body": {$first: "$$ROOT"}
+                            }
+
+                        }], function(err,newResult){
+                            // it should be returning the newest message between user1 and user2
+                            console.log(newResult);
+                            callback(err,newResult);
+                        }
+                    )
                 }
             ], (err, results) => {
                 const firstResult = results[0];
+                const secondResult = results[1];
                 //console.log(firstResult);
-                res.render('privateChat/privateChat', { title: 'Chat-application - Private chat', user: req.user,  data: firstResult });
+                res.render('privateChat/privateChat', { title: 'Chat-application - Private chat', user: req.user,  data: firstResult, pm: secondResult });
             });
         },
+
+
         privateChatPostPage: function(req, res, next){
             const params = req.params.name.split('.');
             const nameParams = params[0];
